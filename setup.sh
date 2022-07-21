43cdff75f64299358f190fbb9b462d1775a92f41
Anirudh Vadakedath <60284251+anirudhv14@users.noreply.github.com>
2022-07-21 15:03:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]
-email = "anirudhv2001@ymail.com"
+echo "\
+[general]\
+email = \"anirudhv2001@ymail.com\"\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]
-headless = true
-enableCORS=false
-port = $PORT
+echo "\
+[server]\
+headless=true\
+enableCORS=false\
+port = $PORT\
 " > ~/.streamlit/config.toml
