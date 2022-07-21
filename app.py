ad3d520c190fc2810c03faf35910ec01c6519c55
Anirudh Vadakedath <60284251+anirudhv14@users.noreply.github.com>
2022-07-21 14:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ st.write("""
 # Addition
 This app adds 2 numbers
 """)
+
+#Get Input
+
+st.header('User Input Parameters')
+
+num1 = st.number_input("a",min_value=-10000000.0,max_value=10000000.0)
+num2 = st.number_input("b",min_value=-10000000.0,max_value=10000000.0)
+
+st.header('Output')
+st.write('a + b = ',str(num1+num2))
