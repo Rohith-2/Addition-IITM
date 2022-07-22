4c7b9d3ca76585dd9da087b5dad8c801cd39204a
rohith <rrohith2001@gmail.com>
2022-07-22 10:56:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
-[general]\
-email = \"anirudhv2001@ymail.com\"\
+[general]\n\
+email = \"rrohith2001@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
-[server]\
-headless=true\
-enableCORS=false\
-port = 8501\
+[server]\n\
+headless=true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
